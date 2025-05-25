edb2eb38e1d56a97b2f3c7bf32773b2dcd7febaa
stefanjelkic00 <stefanjelkic2@gmail.com>
2025-05-25 23:29:44 +0200

    Komit pred call

--- src/app/components/Team.js
+++ src/app/components/Team.js
@@ -8,39 +8,43 @@ export default function Team() {
         <h2 className="text-5xl sm:text-7xl md:text-8xl font-bold text-salon-purple mb-8 animate-on-scroll great-vibes">
           Naš tim
         </h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 sm:gap-8 max-w-7xl mx-auto">
-          <div className="bg-salon-white p-4 sm:p-6 rounded-lg shadow-md animate-on-scroll flex flex-col items-center">
+        <div className="
+          flex flex-row gap-4
+          md:grid md:grid-cols-3 md:gap-8
+          max-w-7xl mx-auto
+        ">
+          <div className="bg-salon-white p-4 md:p-6 rounded-lg shadow-md animate-on-scroll flex flex-col items-center flex-1">
             <Image
               src="/workers/dragana_opsenica.jpg"
               alt="Dragana Ospensica"
               width={250}
               height={350}
-              className="rounded-lg mb-4 object-cover w-[250px] h-[350px]"
+              className="rounded-lg mb-4 object-cover w-full h-[140px] md:w-[250px] md:h-[350px]"
             />
-            <h3 className="text-2xl sm:text-4xl font-bold text-salon-purple great-vibes mb-2">Dragana Ospensica</h3>
-            <p className="text-xl sm:text-2xl text-salon-black great-vibes font-bold mb-2">Osnivačica salona</p>
+            <h3 className="text-2xl md:text-4xl font-bold text-salon-purple great-vibes mb-2">Dragana Opsenica</h3>
+            <p className="text-xl md:text-2xl text-salon-black great-vibes font-bold mb-2">Osnivačica salona</p>
           </div>
-          <div className="bg-salon-white p-4 sm:p-6 rounded-lg shadow-md animate-on-scroll flex flex-col items-center">
+          <div className="bg-salon-white p-4 md:p-6 rounded-lg shadow-md animate-on-scroll flex flex-col items-center flex-1">
             <Image
               src="/workers/milica_jovanovic.jpg"
               alt="Milica Jovanović"
               width={250}
               height={350}
-              className="rounded-lg mb-4 object-cover w-[250px] h-[350px]"
+              className="rounded-lg mb-4 object-cover w-full h-[140px] md:w-[250px] md:h-[350px]"
             />
-            <h3 className="text-2xl sm:text-4xl font-bold text-salon-purple great-vibes mb-2">Milica Jovanović</h3>
-            <p className="text-xl sm:text-2xl text-salon-black great-vibes font-bold mb-2">Frizerski specijalista</p>
+            <h3 className="text-2xl md:text-4xl font-bold text-salon-purple great-vibes mb-2">Milica Jovanović</h3>
+            <p className="text-xl md:text-2xl text-salon-black great-vibes font-bold mb-2">Frizerski specijalista</p>
           </div>
-          <div className="bg-salon-white p-4 sm:p-6 rounded-lg shadow-md animate-on-scroll flex flex-col items-center">
+          <div className="bg-salon-white p-4 md:p-6 rounded-lg shadow-md animate-on-scroll flex flex-col items-center flex-1">
             <Image
               src="/workers/slavica_bavarcic.jpg"
               alt="Slavica Bavaričić"
               width={250}
               height={350}
-              className="rounded-lg mb-4 object-cover w-[250px] h-[350px]"
+              className="rounded-lg mb-4 object-cover w-full h-[140px] md:w-[250px] md:h-[350px]"
             />
-            <h3 className="text-2xl sm:text-4xl font-bold text-salon-purple great-vibes mb-2">Slavica Bavaričić</h3>
-            <p className="text-xl sm:text-2xl text-salon-black great-vibes font-bold mb-2">Frizerski specijalista</p>
+            <h3 className="text-2xl md:text-4xl font-bold text-salon-purple great-vibes mb-2">Slavica Bavaričić</h3>
+            <p className="text-xl md:text-2xl text-salon-black great-vibes font-bold mb-2">Frizerski specijalista</p>
           </div>
         </div>
       </div>
